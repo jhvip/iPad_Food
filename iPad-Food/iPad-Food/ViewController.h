3028b690b99786b6b1298f1522a19beb671e024b
蒋豪 <653513754@qq.com>
2016-03-29 10:58:42 +0800

    初始化项目

---
+++ iPad-Food/iPad-Food/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iPad-Food
+//
+//  Created by 蒋豪 on 16/3/29.
+//  Copyright © 2016年 wpx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iPad-Food/iPad-Food/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iPad-Food
+//
+//  Created by 蒋豪 on 16/3/29.
+//  Copyright © 2016年 wpx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
